3e8b7390b268453d8c640b321152581914e4e429
manuelitox <manuel2788@gmail.com>
2015-06-27 13:57:55 -0430

    adding csswring

--- grunt/postcss.js
+++ grunt/postcss.js
@@ -2,7 +2,8 @@ module.exports = {
   options: {
 		map: true,
 		processors: [
-			require('autoprefixer-core')({browsers: ['last 6 version', 'ie 9', 'ie 10']}).postcss
+			require('autoprefixer-core')({browsers: ['last 6 version', 'ie 9', 'ie 10']}),
+			require('csswring')
 		]
 	},
 	dist: {
